957d28f5927bd73869ee57cd650097fa49fc8769
Albert Terc <albytterc@gmail.com>
2022-06-28 19:44:46 -0400

    Hid API Key

--- youtube.py
+++ youtube.py
@@ -1,8 +1,10 @@
-from pyyoutube import Api
+# from pyyoutube import Api
+import os
 
-API_KEY = "AIzaSyBwmZ4VtYGhYWuJOQvV-gtVUy4P6idf0X4"
+API_KEY = os.environ.get('YT_API_KEY')
 
-api = Api(api_key = API_KEY)
-channel_by_id = api.get_channel_info(channel_id="UC_x5XG1OV2P6uZZ5FSM9Ttw")
+print(API_KEY)
+# api = Api(api_key=API_KEY)
+# channel_by_id = api.get_channel_info(channel_id="UC_x5XG1OV2P6uZZ5FSM9Ttw")
 
-print(channel_by_id.items[0].to_dict())
+# print(channel_by_id.items[0].to_dict())
